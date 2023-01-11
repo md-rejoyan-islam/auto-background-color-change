a7bad90227c60464af0ed9edfcaa4cfd62e22d9f
RejoyanIslam <rejoyanislam0014@gmail.com>
2023-01-11 14:40:18 +0600

    update

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ const body = document.querySelector("body");
 const random = Math.floor(Math.random() * 255);
 const showRgb = document.getElementById("showRgb");
 const showHex = document.getElementById("showHex");
-const copyText = document.getElementById("copyText");
-const show = document.getElementById("show");
 // rgb =()
-
 showRgb.innerHTML = `Color: rgb(255,255,255)`;
 showHex.innerHTML = `Color: #fff`;
 
@@ -15,19 +12,10 @@ setInterval(() => {
   const red = Math.floor(Math.random() * 255);
   const green = Math.floor(Math.random() * 255);
   const blue = Math.floor(Math.random() * 255);
-  RGB = `rgb(${red},${green},${blue})`;
-  HEX = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
-  body.style.backgroundColor = `${RGB}`;
-  showRgb.innerHTML = `Color: rgb(${red},${green},${blue})`;
-  showHex.innerHTML = `Color: ${HEX}`;
-}, 2000);
 
-//copy text
-copyText.onclick = () => {
-  navigator.clipboard.writeText(HEX);
-  show.style.display = "block";
-  show.innerHTML = `copied : ${HEX}`;
-  setInterval(() => {
-    show.style.display = "none";
-  }, 700);
-};
+  body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+  showRgb.innerHTML = `Color: rgb(${red},${green},${blue})`;
+  showHex.innerHTML = `Color: #${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+}, 1000);
+
+
